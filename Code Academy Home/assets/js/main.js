83e49f8ff6f79090e6bc38b2a7be683fe4f3a779
NazMzadeh <100687473+Nz020708@users.noreply.github.com>
2022-05-15 17:02:51 +0400

    Add files via upload

---
+++ Code Academy Home/assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.slick').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 4500,
+        cssEase: 'linear',
+        arrows: true,
+   
+    });
+   
+});
